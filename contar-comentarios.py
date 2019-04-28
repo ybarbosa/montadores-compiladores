cf2db74e88c90cbc81cff35968e2095b9f81072d
Wesley Costa <wesley.costa.developer@gmail.com>
2019-04-28 13:45:20 -0300

    Contar comentarios

---
+++ contar-comentarios.py
@@ -0,0 +1,19 @@
+quantidade = 0
+cont = False
+with open("teste.txt") as arquivo:
+    for linha in arquivo:
+        if cont == False:
+            if '//' in linha:
+                quantidade = quantidade + 1
+            elif '/*' in linha and '*/' in linha:
+                quantidade = quantidade + 1
+            elif '/*' in linha:
+                quantidade = quantidade + 1
+                cont = True
+        else:
+            if '*/' in linha:
+                quantidade = quantidade + 1
+                cont = True
+            else:
+                quantidade = quantidade + 1
+print(quantidade)
